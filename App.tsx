4fbbae3038123af9c83fbc5c050848f6daa833e8
Lucas Augusto <lucasaugustscode@gmail.com>
2022-02-03 00:10:19 -0300

    feat: Defining application fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
-import { View } from 'react-native'
+
+import { StatusBar } from 'expo-status-bar'
+
+import {
+  useFonts,
+  ReadexPro_300Light,
+  ReadexPro_400Regular,
+  ReadexPro_500Medium,
+  ReadexPro_600SemiBold,
+  ReadexPro_700Bold
+} from '@expo-google-fonts/readex-pro'
+import AppLoading from 'expo-app-loading'
+
+import { Home } from '@screens/Home'
 
 const App: React.FC = () => {
-  return <View />
+  const [fontsLoaded] = useFonts({
+    ReadexPro_300Light,
+    ReadexPro_400Regular,
+    ReadexPro_500Medium,
+    ReadexPro_600SemiBold,
+    ReadexPro_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <>
+      <StatusBar style="dark" translucent backgroundColor="transparent" />
+      <Home />
+    </>
+  )
 }
 
 export default App
