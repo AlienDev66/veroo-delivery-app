0a01219b5ceded9b9ded8249695bdb726cc4086c
Lucas Augusto <lucasaugustscode@gmail.com>
2022-02-03 00:04:23 -0300

    feat: Setting the application default theme

---
+++ src/global/themes/default.ts
@@ -0,0 +1,22 @@
+export const theme = {
+  COLORS: {
+    main_light: '#E4F3F7',
+    main_dark: '#54B2CF',
+
+    title: '#323232',
+    text: '#8E8E93',
+
+    highlighter: '#B88AF8',
+
+    shape: '#C7C7CC',
+
+    background: '#FFFFFF'
+  },
+  FONTS: {
+    light: 'ReadexPro_300Light',
+    regular: 'ReadexPro_400Regular',
+    medium: 'ReadexPro_500Medium',
+    semiBold: 'ReadexPro_600SemiBold',
+    bold: 'ReadexPro_700Bold'
+  }
+}
