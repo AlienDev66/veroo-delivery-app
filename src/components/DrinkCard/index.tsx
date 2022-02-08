aa096ea61978b0ed4da726a2c196fecc253e8fd1
Lucas Augusto <lucasaugustscode@gmail.com>
2022-02-08 02:16:48 -0300

    feat: Adding data in the card component

--- src/components/DrinkCard/index.tsx
+++ src/components/DrinkCard/index.tsx
@@ -1,32 +1,62 @@
 import React from 'react'
-import { Image, View, Text } from 'react-native'
+import { View, Text } from 'react-native'
 
 import TouchableScale, {
   TouchableScaleProps
 } from 'react-native-touchable-scale'
+import { SvgFromUri } from 'react-native-svg'
+import { SharedElement } from 'react-navigation-shared-element'
+
+import { MotiView } from 'moti'
 
 import { styles } from './styles'
 
-import exampleImg from '@assets/example.png'
 import AddIcon from '@assets/add.svg'
 
-interface Props extends TouchableScaleProps {}
+interface Props extends TouchableScaleProps {
+  data: {
+    id: string
+    photo: string
+    name: string
+    price: number
+  }
+  delay: number
+}
 
-export const DrinkCard: React.FC<Props> = ({ ...rest }) => {
+export const DrinkCard: React.FC<Props> = ({ data, delay, ...rest }) => {
   return (
-    <TouchableScale style={styles.container} friction={5} {...rest}>
-      <Image source={exampleImg} />
+    <MotiView
+      from={{
+        opacity: 0,
+        translateY: 32
+      }}
+      animate={{
+        opacity: 1,
+        translateY: 0
+      }}
+      delay={delay * 100}
+    >
+      <TouchableScale style={styles.container} friction={5} {...rest}>
+        <SharedElement id={`item.${data.id}.photo`}>
+          <SvgFromUri uri={data.photo} />
+        </SharedElement>
 
-      <View style={styles.content}>
-        <Text style={styles.title} numberOfLines={2} ellipsizeMode="tail">
-          Peppermint Mocha
-        </Text>
+        <View style={styles.content}>
+          <Text style={styles.title} numberOfLines={2} ellipsizeMode="tail">
+            {data.name}
+          </Text>
 
-        <View style={styles.footer}>
-          <Text style={styles.price}>$ 6.99</Text>
-          <AddIcon />
+          <View style={styles.footer}>
+            <Text style={styles.price}>
+              {data.price.toLocaleString('es-US', {
+                style: 'currency',
+                currency: 'USD'
+              })}
+            </Text>
+            <AddIcon />
+          </View>
         </View>
-      </View>
-    </TouchableScale>
+      </TouchableScale>
+    </MotiView>
   )
 }
