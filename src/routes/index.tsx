aae3e99a775f6616382e6021693c3a1d0ea07c11
Lucas Augusto <lucasaugustscode@gmail.com>
2022-02-08 02:15:39 -0300

    feat: Defining application routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
-
-import { theme } from '@global/themes/default'
 
 import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-
-import { RFValue } from 'react-native-responsive-fontsize'
+import { createSharedElementStackNavigator } from 'react-navigation-shared-element'
 
 import { Home } from '@screens/Home'
+import { Details } from '@screens/Details'
 
-import HomeIcon from '@assets/home.svg'
-import SearchIcon from '@assets/search.svg'
-import BagIcon from '@assets/bag.svg'
+type RootStackParamList = {
+  Home: any
+  Details: any
+}
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
 
-const { Navigator, Screen } = createBottomTabNavigator()
+const { Navigator, Screen } = createSharedElementStackNavigator()
 
 export const Routes: React.FC = () => {
   return (
@@ -22,91 +24,48 @@ export const Routes: React.FC = () => {
       <Navigator
         screenOptions={{
           headerShown: false,
-          tabBarShowLabel: false,
-          tabBarStyle: {
-            height: RFValue(80),
-            alignItems: 'center',
-            paddingHorizontal: 52
+          transitionSpec: {
+            open: { animation: 'timing', config: { duration: 300 } },
+            close: { animation: 'timing', config: { duration: 300 } }
           },
-          tabBarItemStyle: {
-            maxWidth: 50,
-            maxHeight: 50,
-            marginHorizontal: 56
-          },
-          tabBarActiveTintColor: theme.COLORS.title,
-          tabBarInactiveTintColor: theme.COLORS.shape
+          cardStyleInterpolator: ({ current: { progress } }) => {
+            return {
+              cardStyle: {
+                opacity: progress
+              }
+            }
+          }
         }}
       >
+        <Screen name="Home" component={Home} />
         <Screen
-          name="Home-1"
-          component={Home}
-          options={{
-            tabBarIcon: ({ focused, color }) => (
-              <View style={styles.container}>
-                {focused && <View style={styles.highlighter} />}
-                <HomeIcon width={28} height={28} fill={color} />
-              </View>
-            )
+          name="Details"
+          component={Details}
+          sharedElements={route => {
+            const { item } = route.params
+            return [
+              {
+                id: `item.${item.id}.photo`,
+                animation: 'move',
+                resize: 'auto'
+              },
+              {
+                id: `item.${item.name}.name`,
+                animation: 'move',
+                resize: 'auto'
+              },
+              {
+                id: `item.${item.price}.price`,
+                animation: 'move',
+                resize: 'auto'
+              }
+            ]
           }}
-        />
-        <Screen
-          name="Home-2"
-          component={Home}
           options={{
-            tabBarIcon: ({ focused, color }) => (
-              <View style={styles.container}>
-                {focused && <View style={styles.highlighter} />}
-                <SearchIcon
-                  width={28}
-                  height={28}
-                  stroke={color}
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </View>
-            )
-          }}
-        />
-        <Screen
-          name="Home-3"
-          component={Home}
-          options={{
-            tabBarIcon: ({ focused, color }) => (
-              <View style={styles.container}>
-                {focused && <View style={styles.highlighter} />}
-                <BagIcon
-                  width={28}
-                  height={28}
-                  stroke={color}
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </View>
-            )
+            gestureEnabled: false
           }}
         />
       </Navigator>
     </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: 50,
-    height: 50,
-
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  highlighter: {
-    width: 18,
-    height: 3,
-
-    position: 'absolute',
-    top: 0,
-
-    backgroundColor: theme.COLORS.main_dark
-  }
-})
